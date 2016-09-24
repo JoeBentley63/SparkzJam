cc3100d332ad3496c56aa5d78d7cc56df594cff7
Student <Student>
2016-09-24 11:10:29 +0100

    adding basic character movement

---
+++ SparksJam/Assets/src/scripts/player.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+	public Color color;
+	public float runVelocity;
+	public float jumpVelocity;
+	public float dashVelocity;
+	public float maxSpeed;
+
+	private Rigidbody2D playerRigidbody;
+	// Use this for initialization
+	void Start () {
+		playerRigidbody = this.gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow)){
+			//playerRigidbody.velocity = new Vector2(0, playerRigidbody.velocity.y);
+		}
+		playerRigidbody.AddForce(new Vector3(Input.GetAxis("Horizontal") * runVelocity, 0, 0));
+
+		if(Input.GetKey(KeyCode.DownArrow)){
+			playerRigidbody.AddForce(new Vector3(0, -dashVelocity, 0));
+		}
+
+		else if(Input.GetKeyDown(KeyCode.Space)){
+			playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, 0);
+			playerRigidbody.AddForce(new Vector3(0, jumpVelocity, 0));
+		}
+
+		CapMaxSpeed();
+	}
+
+	void CapMaxSpeed(){
+		if(playerRigidbody.velocity.x > maxSpeed){
+			playerRigidbody.velocity = new Vector2(maxSpeed, playerRigidbody.velocity.y);
+		}
+		else if(playerRigidbody.velocity.x < -maxSpeed){
+			playerRigidbody.velocity = new Vector2(-maxSpeed, playerRigidbody.velocity.y);
+		}
+	}
+}
